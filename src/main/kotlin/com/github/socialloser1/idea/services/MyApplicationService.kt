1fcd9d97e1ef51d569f02df165ca1e06f053e77e
GitHub Action <action@github.com>
2020-11-18 05:24:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/socialloser1/idea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/socialloser1/idea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.socialloser1.idea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.socialloser1.idea.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/socialloser1/idea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/socialloser1/idea/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.socialloser1.idea.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.socialloser1.idea.MyBundle
 
 class MyProjectService(project: Project) {
 
